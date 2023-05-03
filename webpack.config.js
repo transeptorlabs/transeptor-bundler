c282a6cc0a1efe046349bc14b9fcab7de22f8c58
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2023-05-03 19:41:16 -0400

    add webpack build

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/execute.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundler.js'
+  },
+  mode: 'production',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  stats: 'errors-only'
+};
