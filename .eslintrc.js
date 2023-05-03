ddc8c18fb013a88c3aab28be590026e90c8a34f9
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2023-05-03 19:59:01 -0400

    add lint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+  },
+  ignorePatterns: ['node_modules', 'dist', '**/*.js'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      excludedFiles: 'dist/**',
+    },
+  ],
+}
