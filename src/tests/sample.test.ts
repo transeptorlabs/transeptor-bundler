7205674d300714ee12a86949658c5386417aa89b
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2023-05-03 20:14:43 -0400

    add jest for testing

---
+++ src/tests/sample.test.ts
@@ -0,0 +1,5 @@
+describe('Test the root path', () => {
+  test('It should response the GET method', async () => {
+    expect(true).toEqual(true);
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -20,5 +20,5 @@ module.exports = {
       }
     ]
   },
-  stats: 'errors-only'
+  stats: 'errors-only',
 };
