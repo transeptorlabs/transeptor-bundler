2e5440fa91630b6a54fa666f99a610cd7ae51eb5
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2023-05-04 17:00:08 -0400

    add architecture diagram

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        exclude: /node_modules/,
+        exclude: /node_modules|\.test\.ts$/,
         use: 'ts-loader'
       }
     ]
