1f555ef5a62bcc7452e46f78de2ea0dd755e06da
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2023-05-28 15:43:06 -0400

    test fails

--- test/BundleManager.test.ts
+++ test/BundleManager.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 
-describe('BundleManager', () => {
+describe("BundleManager", () => {
     it('placeholder', async () => {
-        expect(true).to.eq(true)
+        expect(true).to.eq(false)
     })
 })
